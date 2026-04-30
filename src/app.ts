a37fe6b7691e87519fa69dc1fe9813b74abed6bb
ahanaf607307 <ahanaf607307@gmail.com>
2026-04-30 16:36:58 +0600

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,16 @@ import requestLogger from "./app/middlewares/requestLogger";
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://test4.fireai.agency",
+      "http://localhost:3000",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(requestLogger);
 app.use("/uploads", express.static("uploads"));
